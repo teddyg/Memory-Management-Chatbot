e0893d362b193489c9fa2a5549d6e7c245899099
teddyg <tgulezian@hotmail.com>
2021-03-15 15:04:14 -0400

    initial checkin with 3 tasks completed and #4 in progress

---
+++ src/graphnode.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+    // delete _chatBot; 
+    std::cout << "GraphNode Destructor Called" << std::endl;
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    //_childEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.emplace_back(edge);
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+{
+    _chatBot = chatbot;
+    _chatBot->SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+    _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return (_childEdges[index]).get();
+
+    ////
+    //// EOF STUDENT CODE
+}
